b54bd191cc3292457d3f390b5bbde1292dd50601
Christopher W. Blake <christopher.blake@bakerhughes.com>
2022-05-25 14:36:17 -0500

    Initial commit with Auth workflow enabled using MongoDB

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+using HackathonWebApp.Models;
+
+namespace HackathonWebApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        // Fields
+        private UserManager<ApplicationUser> userManager;
+        private SignInManager<ApplicationUser> signInManager;
+
+        // Constructor
+        public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+        }
+
+        // Methods - Login/Logout
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login([Required][EmailAddress] string email, [Required] string password, string returnurl)
+        {
+            if (ModelState.IsValid)
+            {
+                ApplicationUser appUser = await userManager.FindByEmailAsync(email);
+                if (appUser != null)
+                {
+                    Microsoft.AspNetCore.Identity.SignInResult result = await signInManager.PasswordSignInAsync(appUser, password, false, false);
+                    if (result.Succeeded)
+                    {
+                        return Redirect(returnurl ?? "/");
+                    }
+                }
+                ModelState.AddModelError(nameof(email), "Login Failed: Invalid Email or Password");
+            }
+
+            return View();
+        }
+        [Authorize]
+        public async Task<IActionResult> Logout()
+        {
+            await signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        }
+
+        // Access Denied
+        public IActionResult AccessDenied()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/ApplicationUser.cs
@@ -0,0 +1,11 @@
+﻿using AspNetCore.Identity.MongoDbCore.Models;
+using MongoDbGenericRepository.Attributes;
+using System;
+
+namespace HackathonWebApp.Models
+{
+    [CollectionName("Users")]
+    public class ApplicationUser : MongoIdentityUser<Guid>
+    {
+    }
+}
