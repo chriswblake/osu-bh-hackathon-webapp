53b8bbf6524c41a298ba72f15f5b74b98c99aed4
Christopher W. Blake <christopher.blake@bakerhughes.com>
2022-05-30 14:17:33 -0500

    Added support and pages to manage roles

---
+++ Models/RoleEdit.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Identity;
+
+namespace HackathonWebApp.Models
+{
+    public class RoleEdit
+    {
+        public ApplicationRole Role { get; set; }
+        public IEnumerable<ApplicationUser> Members { get; set; }
+        public IEnumerable<ApplicationUser> NonMembers { get; set; }
+    }
+}
