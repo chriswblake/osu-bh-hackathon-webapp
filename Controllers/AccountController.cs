d91af81a88b1af032f1c135d3962d6fbfd8d7fa3
Christopher W. Blake <christopher.blake@bakerhughes.com>
2022-05-30 16:02:33 -0500

    Added page to manage personal account

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.ComponentModel.DataAnnotations;
@@ -21,6 +21,13 @@ namespace HackathonWebApp.Controllers
         }
 
         // Views
+        [Authorize]
+        public ViewResult Index()
+        {
+            string userName = User.Identity.Name;
+            ApplicationUser appUser = userManager.FindByNameAsync(userName).Result;
+            return View(appUser);
+        }
         public IActionResult Login()
         {
             return View();
@@ -58,6 +65,31 @@ namespace HackathonWebApp.Controllers
             return View(user);
         }
 
+        [Authorize]
+        [HttpPost]
+        public async Task<IActionResult> Delete(string empname)
+        {
+            ApplicationUser appUser = await userManager.FindByNameAsync(User.Identity.Name);
+
+            // Delete the user
+            if (appUser != null)
+            {
+                IdentityResult result = await userManager.DeleteAsync(appUser);
+                if (result.Succeeded)
+                {
+                    // Sign out and display confirmation
+                    await signInManager.SignOutAsync();
+                    return View();
+                }
+                else
+                    Errors(result);
+                }
+            else
+                ModelState.AddModelError("", "No role found");
+
+            return RedirectToAction("Index");
+        }
+
         [HttpPost]
         [AllowAnonymous]
         [ValidateAntiForgeryToken]
@@ -85,6 +117,12 @@ namespace HackathonWebApp.Controllers
         {
             await signInManager.SignOutAsync();
             return RedirectToAction("Index", "Home");
+        }
+
+        private void Errors(IdentityResult result)
+        {
+            foreach (IdentityError error in result.Errors)
+                ModelState.AddModelError("", error.Description);
         }
     }
 }
