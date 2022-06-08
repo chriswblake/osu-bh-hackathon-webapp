348c11e5f276257f4c29ab80071d0e675c8993e7
Christopher W. Blake <christopher.blake@bakerhughes.com>
2022-06-08 13:24:19 -0500

    bugfix: removed runtime compilation

--- Startup.cs
+++ Startup.cs
@@ -73,7 +73,7 @@ namespace HackathonWebApp
             });
 
             // Enable serving web pages
-            services.AddControllersWithViews().AddRazorRuntimeCompilation();
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
