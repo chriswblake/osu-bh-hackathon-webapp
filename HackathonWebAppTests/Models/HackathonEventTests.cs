caa5f2524d203af8c82871852b6b3dae060b6310
Christopher W. Blake <christopher.blake@bakerhughes.com>
2022-07-21 10:15:38 -0500

    Added sample dataset for testing team placement algorithm

--- HackathonWebAppTests/Models/HackathonEventTests.cs
+++ HackathonWebAppTests/Models/HackathonEventTests.cs
@@ -1,4 +1,5 @@
 using System;
+using System.IO;
 using System.Collections.Generic;
 using Xunit;
 using MongoDB.Bson;
@@ -14,68 +15,31 @@ namespace HackathonWebAppTests
             /// <summary>
             /// Description: This set of event applications is meant to represent applications to attend a hackathon event. It is used for testing the team balancing algorithm.
             /// </summary> 
-            public static List<EventApplication> SampleEventApplications = new List<EventApplication> { 
-                new EventApplication(){ Id = ObjectId.Parse("000000000000000000000001"), UserId = Guid.NewGuid(), HackathonExperience = 1, CodingExperience = 1, CommunicationExperience = 1, OrganizationExperience = 1, DocumentationExperience = 1, BusinessExperience = 3, CreativityExperience = 0},
-                new EventApplication(){ Id = ObjectId.Parse("000000000000000000000001"), UserId = Guid.NewGuid(), HackathonExperience = 1, CodingExperience = 1, CommunicationExperience = 1, OrganizationExperience = 1, DocumentationExperience = 1, BusinessExperience = 3, CreativityExperience = 0},
-                new EventApplication(){ Id = ObjectId.Parse("000000000000000000000001"), UserId = Guid.NewGuid(), HackathonExperience = 1, CodingExperience = 1, CommunicationExperience = 1, OrganizationExperience = 1, DocumentationExperience = 1, BusinessExperience = 3, CreativityExperience = 0},
-                new EventApplication(){ Id = ObjectId.Parse("000000000000000000000001"), UserId = Guid.NewGuid(), HackathonExperience = 1, CodingExperience = 1, CommunicationExperience = 1, OrganizationExperience = 1, DocumentationExperience = 1, BusinessExperience = 3, CreativityExperience = 0},
-                new EventApplication(){ Id = ObjectId.Parse("000000000000000000000001"), UserId = Guid.NewGuid(), HackathonExperience = 1, CodingExperience = 1, CommunicationExperience = 1, OrganizationExperience = 1, DocumentationExperience = 1, BusinessExperience = 3, CreativityExperience = 0},
-                
-                new EventApplication(){ Id = ObjectId.Parse("000000000000000000000001"), UserId = Guid.NewGuid(), HackathonExperience = 1, CodingExperience = 1, CommunicationExperience = 1, OrganizationExperience = 1, DocumentationExperience = 1, BusinessExperience = 3, CreativityExperience = 0},
-                new EventApplication(){ Id = ObjectId.Parse("000000000000000000000001"), UserId = Guid.NewGuid(), HackathonExperience = 1, CodingExperience = 1, CommunicationExperience = 1, OrganizationExperience = 1, DocumentationExperience = 1, BusinessExperience = 3, CreativityExperience = 0},
-                new EventApplication(){ Id = ObjectId.Parse("000000000000000000000001"), UserId = Guid.NewGuid(), HackathonExperience = 1, CodingExperience = 1, CommunicationExperience = 1, OrganizationExperience = 1, DocumentationExperience = 1, BusinessExperience = 3, CreativityExperience = 0},
-                new EventApplication(){ Id = ObjectId.Parse("000000000000000000000001"), UserId = Guid.NewGuid(), HackathonExperience = 1, CodingExperience = 1, CommunicationExperience = 1, OrganizationExperience = 1, DocumentationExperience = 1, BusinessExperience = 3, CreativityExperience = 0},
-                new EventApplication(){ Id = ObjectId.Parse("000000000000000000000001"), UserId = Guid.NewGuid(), HackathonExperience = 1, CodingExperience = 1, CommunicationExperience = 1, OrganizationExperience = 1, DocumentationExperience = 1, BusinessExperience = 3, CreativityExperience = 0},
-                
-                new EventApplication(){ Id = ObjectId.Parse("000000000000000000000001"), UserId = Guid.NewGuid(), HackathonExperience = 1, CodingExperience = 1, CommunicationExperience = 1, OrganizationExperience = 1, DocumentationExperience = 1, BusinessExperience = 3, CreativityExperience = 0},
-                new EventApplication(){ Id = ObjectId.Parse("000000000000000000000001"), UserId = Guid.NewGuid(), HackathonExperience = 1, CodingExperience = 1, CommunicationExperience = 1, OrganizationExperience = 1, DocumentationExperience = 1, BusinessExperience = 3, CreativityExperience = 0},
-                new EventApplication(){ Id = ObjectId.Parse("000000000000000000000001"), UserId = Guid.NewGuid(), HackathonExperience = 1, CodingExperience = 1, CommunicationExperience = 1, OrganizationExperience = 1, DocumentationExperience = 1, BusinessExperience = 3, CreativityExperience = 0},
-                new EventApplication(){ Id = ObjectId.Parse("000000000000000000000001"), UserId = Guid.NewGuid(), HackathonExperience = 1, CodingExperience = 1, CommunicationExperience = 1, OrganizationExperience = 1, DocumentationExperience = 1, BusinessExperience = 3, CreativityExperience = 0},
-                new EventApplication(){ Id = ObjectId.Parse("000000000000000000000001"), UserId = Guid.NewGuid(), HackathonExperience = 1, CodingExperience = 1, CommunicationExperience = 1, OrganizationExperience = 1, DocumentationExperience = 1, BusinessExperience = 3, CreativityExperience = 0},
-                
-                new EventApplication(){ Id = ObjectId.Parse("000000000000000000000001"), UserId = Guid.NewGuid(), HackathonExperience = 1, CodingExperience = 1, CommunicationExperience = 1, OrganizationExperience = 1, DocumentationExperience = 1, BusinessExperience = 3, CreativityExperience = 0},
-                new EventApplication(){ Id = ObjectId.Parse("000000000000000000000001"), UserId = Guid.NewGuid(), HackathonExperience = 1, CodingExperience = 1, CommunicationExperience = 1, OrganizationExperience = 1, DocumentationExperience = 1, BusinessExperience = 3, CreativityExperience = 0},
-                new EventApplication(){ Id = ObjectId.Parse("000000000000000000000001"), UserId = Guid.NewGuid(), HackathonExperience = 1, CodingExperience = 1, CommunicationExperience = 1, OrganizationExperience = 1, DocumentationExperience = 1, BusinessExperience = 3, CreativityExperience = 0},
-                new EventApplication(){ Id = ObjectId.Parse("000000000000000000000001"), UserId = Guid.NewGuid(), HackathonExperience = 1, CodingExperience = 1, CommunicationExperience = 1, OrganizationExperience = 1, DocumentationExperience = 1, BusinessExperience = 3, CreativityExperience = 0},
-                new EventApplication(){ Id = ObjectId.Parse("000000000000000000000001"), UserId = Guid.NewGuid(), HackathonExperience = 1, CodingExperience = 1, CommunicationExperience = 1, OrganizationExperience = 1, DocumentationExperience = 1, BusinessExperience = 3, CreativityExperience = 0},
-                
-                new EventApplication(){ Id = ObjectId.Parse("000000000000000000000001"), UserId = Guid.NewGuid(), HackathonExperience = 1, CodingExperience = 1, CommunicationExperience = 1, OrganizationExperience = 1, DocumentationExperience = 1, BusinessExperience = 3, CreativityExperience = 0},
-                new EventApplication(){ Id = ObjectId.Parse("000000000000000000000001"), UserId = Guid.NewGuid(), HackathonExperience = 1, CodingExperience = 1, CommunicationExperience = 1, OrganizationExperience = 1, DocumentationExperience = 1, BusinessExperience = 3, CreativityExperience = 0},
-                new EventApplication(){ Id = ObjectId.Parse("000000000000000000000001"), UserId = Guid.NewGuid(), HackathonExperience = 1, CodingExperience = 1, CommunicationExperience = 1, OrganizationExperience = 1, DocumentationExperience = 1, BusinessExperience = 3, CreativityExperience = 0},
-                new EventApplication(){ Id = ObjectId.Parse("000000000000000000000001"), UserId = Guid.NewGuid(), HackathonExperience = 1, CodingExperience = 1, CommunicationExperience = 1, OrganizationExperience = 1, DocumentationExperience = 1, BusinessExperience = 3, CreativityExperience = 0},
-                new EventApplication(){ Id = ObjectId.Parse("000000000000000000000001"), UserId = Guid.NewGuid(), HackathonExperience = 1, CodingExperience = 1, CommunicationExperience = 1, OrganizationExperience = 1, DocumentationExperience = 1, BusinessExperience = 3, CreativityExperience = 0},
-                
-                new EventApplication(){ Id = ObjectId.Parse("000000000000000000000001"), UserId = Guid.NewGuid(), HackathonExperience = 1, CodingExperience = 1, CommunicationExperience = 1, OrganizationExperience = 1, DocumentationExperience = 1, BusinessExperience = 3, CreativityExperience = 0},
-                new EventApplication(){ Id = ObjectId.Parse("000000000000000000000001"), UserId = Guid.NewGuid(), HackathonExperience = 1, CodingExperience = 1, CommunicationExperience = 1, OrganizationExperience = 1, DocumentationExperience = 1, BusinessExperience = 3, CreativityExperience = 0},
-                new EventApplication(){ Id = ObjectId.Parse("000000000000000000000001"), UserId = Guid.NewGuid(), HackathonExperience = 1, CodingExperience = 1, CommunicationExperience = 1, OrganizationExperience = 1, DocumentationExperience = 1, BusinessExperience = 3, CreativityExperience = 0},
-                new EventApplication(){ Id = ObjectId.Parse("000000000000000000000001"), UserId = Guid.NewGuid(), HackathonExperience = 1, CodingExperience = 1, CommunicationExperience = 1, OrganizationExperience = 1, DocumentationExperience = 1, BusinessExperience = 3, CreativityExperience = 0},
-                new EventApplication(){ Id = ObjectId.Parse("000000000000000000000001"), UserId = Guid.NewGuid(), HackathonExperience = 1, CodingExperience = 1, CommunicationExperience = 1, OrganizationExperience = 1, DocumentationExperience = 1, BusinessExperience = 3, CreativityExperience = 0},
-                
-                new EventApplication(){ Id = ObjectId.Parse("000000000000000000000001"), UserId = Guid.NewGuid(), HackathonExperience = 1, CodingExperience = 1, CommunicationExperience = 1, OrganizationExperience = 1, DocumentationExperience = 1, BusinessExperience = 3, CreativityExperience = 0},
-                new EventApplication(){ Id = ObjectId.Parse("000000000000000000000001"), UserId = Guid.NewGuid(), HackathonExperience = 1, CodingExperience = 1, CommunicationExperience = 1, OrganizationExperience = 1, DocumentationExperience = 1, BusinessExperience = 3, CreativityExperience = 0},
-                new EventApplication(){ Id = ObjectId.Parse("000000000000000000000001"), UserId = Guid.NewGuid(), HackathonExperience = 1, CodingExperience = 1, CommunicationExperience = 1, OrganizationExperience = 1, DocumentationExperience = 1, BusinessExperience = 3, CreativityExperience = 0},
-                new EventApplication(){ Id = ObjectId.Parse("000000000000000000000001"), UserId = Guid.NewGuid(), HackathonExperience = 1, CodingExperience = 1, CommunicationExperience = 1, OrganizationExperience = 1, DocumentationExperience = 1, BusinessExperience = 3, CreativityExperience = 0},
-                new EventApplication(){ Id = ObjectId.Parse("000000000000000000000001"), UserId = Guid.NewGuid(), HackathonExperience = 1, CodingExperience = 1, CommunicationExperience = 1, OrganizationExperience = 1, DocumentationExperience = 1, BusinessExperience = 3, CreativityExperience = 0},
-                
-                new EventApplication(){ Id = ObjectId.Parse("000000000000000000000001"), UserId = Guid.NewGuid(), HackathonExperience = 1, CodingExperience = 1, CommunicationExperience = 1, OrganizationExperience = 1, DocumentationExperience = 1, BusinessExperience = 3, CreativityExperience = 0},
-                new EventApplication(){ Id = ObjectId.Parse("000000000000000000000001"), UserId = Guid.NewGuid(), HackathonExperience = 1, CodingExperience = 1, CommunicationExperience = 1, OrganizationExperience = 1, DocumentationExperience = 1, BusinessExperience = 3, CreativityExperience = 0},
-                new EventApplication(){ Id = ObjectId.Parse("000000000000000000000001"), UserId = Guid.NewGuid(), HackathonExperience = 1, CodingExperience = 1, CommunicationExperience = 1, OrganizationExperience = 1, DocumentationExperience = 1, BusinessExperience = 3, CreativityExperience = 0},
-                new EventApplication(){ Id = ObjectId.Parse("000000000000000000000001"), UserId = Guid.NewGuid(), HackathonExperience = 1, CodingExperience = 1, CommunicationExperience = 1, OrganizationExperience = 1, DocumentationExperience = 1, BusinessExperience = 3, CreativityExperience = 0},
-                new EventApplication(){ Id = ObjectId.Parse("000000000000000000000001"), UserId = Guid.NewGuid(), HackathonExperience = 1, CodingExperience = 1, CommunicationExperience = 1, OrganizationExperience = 1, DocumentationExperience = 1, BusinessExperience = 3, CreativityExperience = 0},
-                
-                new EventApplication(){ Id = ObjectId.Parse("000000000000000000000001"), UserId = Guid.NewGuid(), HackathonExperience = 1, CodingExperience = 1, CommunicationExperience = 1, OrganizationExperience = 1, DocumentationExperience = 1, BusinessExperience = 3, CreativityExperience = 0},
-                new EventApplication(){ Id = ObjectId.Parse("000000000000000000000001"), UserId = Guid.NewGuid(), HackathonExperience = 1, CodingExperience = 1, CommunicationExperience = 1, OrganizationExperience = 1, DocumentationExperience = 1, BusinessExperience = 3, CreativityExperience = 0},
-                new EventApplication(){ Id = ObjectId.Parse("000000000000000000000001"), UserId = Guid.NewGuid(), HackathonExperience = 1, CodingExperience = 1, CommunicationExperience = 1, OrganizationExperience = 1, DocumentationExperience = 1, BusinessExperience = 3, CreativityExperience = 0},
-                new EventApplication(){ Id = ObjectId.Parse("000000000000000000000001"), UserId = Guid.NewGuid(), HackathonExperience = 1, CodingExperience = 1, CommunicationExperience = 1, OrganizationExperience = 1, DocumentationExperience = 1, BusinessExperience = 3, CreativityExperience = 0},
-                new EventApplication(){ Id = ObjectId.Parse("000000000000000000000001"), UserId = Guid.NewGuid(), HackathonExperience = 1, CodingExperience = 1, CommunicationExperience = 1, OrganizationExperience = 1, DocumentationExperience = 1, BusinessExperience = 3, CreativityExperience = 0},
-                
-                new EventApplication(){ Id = ObjectId.Parse("000000000000000000000001"), UserId = Guid.NewGuid(), HackathonExperience = 1, CodingExperience = 1, CommunicationExperience = 1, OrganizationExperience = 1, DocumentationExperience = 1, BusinessExperience = 3, CreativityExperience = 0},
-                new EventApplication(){ Id = ObjectId.Parse("000000000000000000000001"), UserId = Guid.NewGuid(), HackathonExperience = 1, CodingExperience = 1, CommunicationExperience = 1, OrganizationExperience = 1, DocumentationExperience = 1, BusinessExperience = 3, CreativityExperience = 0},
-                new EventApplication(){ Id = ObjectId.Parse("000000000000000000000001"), UserId = Guid.NewGuid(), HackathonExperience = 1, CodingExperience = 1, CommunicationExperience = 1, OrganizationExperience = 1, DocumentationExperience = 1, BusinessExperience = 3, CreativityExperience = 0},
-                new EventApplication(){ Id = ObjectId.Parse("000000000000000000000001"), UserId = Guid.NewGuid(), HackathonExperience = 1, CodingExperience = 1, CommunicationExperience = 1, OrganizationExperience = 1, DocumentationExperience = 1, BusinessExperience = 3, CreativityExperience = 0},
-                new EventApplication(){ Id = ObjectId.Parse("000000000000000000000001"), UserId = Guid.NewGuid(), HackathonExperience = 1, CodingExperience = 1, CommunicationExperience = 1, OrganizationExperience = 1, DocumentationExperience = 1, BusinessExperience = 3, CreativityExperience = 0}
-            };
-
+            public static List<EventApplication> SampleEventApplications_Distributed { get {
+                var eventApplications = new List<EventApplication>();
+                string csvFileLocation = Path.Combine(Directory.GetCurrentDirectory(), "Models", "SampleData", "EventApplications-Distributed.csv");
+                using(var reader = new StreamReader(csvFileLocation))
+                {
+                    var header = reader.ReadLine();
+                    while (!reader.EndOfStream)
+                    {
+                        var line = reader.ReadLine();
+                        var values = line.Split(',');
+                        eventApplications.Add( new EventApplication(){
+                            Id = ObjectId.GenerateNewId(),
+                            UserId = Guid.NewGuid(),
+                            HackathonExperience = Convert.ToInt32(values[1]),
+                            CodingExperience = Convert.ToInt32(values[2]),
+                            CommunicationExperience = Convert.ToInt32(values[3]),
+                            OrganizationExperience = Convert.ToInt32(values[4]),
+                            DocumentationExperience = Convert.ToInt32(values[5]),
+                            BusinessExperience = Convert.ToInt32(values[6]),
+                            CreativityExperience = Convert.ToInt32(values[7])
+                        });
+                    }
+                }                
+                return eventApplications;
+            }}
         }
 
         /// <summary>
@@ -86,7 +50,7 @@ namespace HackathonWebAppTests
         {
             // Define
             var hackathonEvent = new HackathonEvent() {Name = "HackOkState" };
-            var eventApplications = SampleData.SampleEventApplications;
+            var eventApplications = SampleData.SampleEventApplications_Distributed;
             var numTeams = 10;
 
             // Process
