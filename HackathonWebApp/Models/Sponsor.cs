108a539d4e0a6d50c259ebf5d9cb4178f1c8bd39
Christopher W. Blake <christopher.blake@bakerhughes.com>
2022-07-26 16:45:19 -0500

    Added medal to sponsors that have consecutive years of support.

--- HackathonWebApp/Models/Sponsor.cs
+++ HackathonWebApp/Models/Sponsor.cs
@@ -13,6 +13,9 @@ namespace HackathonWebApp.Models
 
         [BsonElement("tier")]
         public string Tier { get; set; }
+
+        [BsonElement("years_consecutive_support")]
+        public int YearsConsecutiveSuppport { get; set; }
         
         [BsonElement("website_url")]
         public string WebsiteUrl { get; set; }
